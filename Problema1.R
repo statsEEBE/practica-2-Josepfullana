188f986733a0b08a37cde04b5b390f410954d2b3
josep.fullana <josep.fullana@auleseebe.upc.edu>
2024-09-26 11:28:55 +0200

    Codigo para el segundo ejercicio

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,22 @@
 #Codigo para problema 1
 
+
+#b)
+sqrt(0.1923)
+  #0.4385202
+
+#c) nos preguntan la m de la recta
+  #0.06576 se pilla del enunciado
+
+#d)nos preguntan una estimacion de y
+  #construimos la recta y=mx+b->y=0,065*x+11.79755 usando x = 647.1
+
+y = 0.06576*647.1+11.79755
+y
+    # y = 54.35085
+
+#e) con el valor predicho escojer una muestra de las imagenes
+
+
+
+
